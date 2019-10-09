1378d5c071dd3ca9dea834fff5d180fe038eb6f4
Ali Tourani <a.tourani1991@gmail.com>
2019-10-10 01:24:44 +0330

    feat: provide a basic form of application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,160 @@
 import React from 'react';
+import { Button, Typography, Grid, List, ListItem, ListItemIcon, ListItemText, Checkbox, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: 'auto',
+  },
+  paper: {
+    width: 200,
+    height: 230,
+    overflow: 'auto',
+  },
+  button: {
+    margin: theme.spacing(0.5, 0),
+  },
+}));
+
+function not(a, b) {
+  return a.filter(value => b.indexOf(value) === -1);
+}
+
+function intersection(a, b) {
+  return a.filter(value => b.indexOf(value) !== -1);
+}
+
+export default function TransferList() {
+  const classes = useStyles();
+  const [checked, setChecked] = React.useState([]);
+  const [left, setLeft] = React.useState([0, 1, 2, 3]);
+  const [right, setRight] = React.useState([4, 5, 6, 7]);
+
+  const leftChecked = intersection(checked, left);
+  const rightChecked = intersection(checked, right);
+
+  const handleToggle = value => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const handleAllRight = () => {
+    setRight(right.concat(left));
+    setLeft([]);
+  };
+
+  const handleCheckedRight = () => {
+    setRight(right.concat(leftChecked));
+    setLeft(not(left, leftChecked));
+    setChecked(not(checked, leftChecked));
+  };
+
+  const handleCheckedLeft = () => {
+    setLeft(left.concat(rightChecked));
+    setRight(not(right, rightChecked));
+    setChecked(not(checked, rightChecked));
+  };
+
+  const handleAllLeft = () => {
+    setLeft(left.concat(right));
+    setRight([]);
+  };
+
+  const customList = items => (
+    <Paper className={classes.paper}>
+      <List dense component="div" role="list">
+        {items.map(value => {
+          const labelId = `transfer-list-item-${value}-label`;
+
+          return (
+            <ListItem key={value} role="listitem" button onClick={handleToggle(value)}>
+              <ListItemIcon>
+                <Checkbox
+                  checked={checked.indexOf(value) !== -1}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={`List item ${value + 1}`} />
+            </ListItem>
+          );
+        })}
+        <ListItem />
+      </List>
+    </Paper>
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a simple React.js application :)
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Grid container className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Typography variant="h4" gutterBottom>
+      This is a simple program to simplify our draw demands
+      </Typography>
+      <Button variant="contained" href="#contained-buttons">
+      Click to Draw
+      </Button>
+    
+    <Grid container spacing={2} justify="center" alignItems="center" className={classes.root}>
+      <Grid item>{customList(left)}</Grid>
+      <Grid item>
+        <Grid container direction="column" alignItems="center">
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleAllRight}
+            disabled={left.length === 0}
+            aria-label="move all right"
+          >
+            ≫
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleCheckedRight}
+            disabled={leftChecked.length === 0}
+            aria-label="move selected right"
+          >
+            &gt;
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleCheckedLeft}
+            disabled={rightChecked.length === 0}
+            aria-label="move selected left"
+          >
+            &lt;
+          </Button>
+          <Button
+            variant="outlined"
+            size="small"
+            className={classes.button}
+            onClick={handleAllLeft}
+            disabled={right.length === 0}
+            aria-label="move all left"
+          >
+            ≪
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid item>{customList(right)}</Grid>
+    </Grid>
+    </Grid>  
     </div>
   );
 }
-
-export default App;
